5716e2dadae464c65d5f7235d4df7f84049f4679
Mht70 <albakovmagomed1@gmail.com>
2021-11-15 21:42:26 +0300

    test

---
+++ src/main/java/p4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class p4 {
+
+    public static void main(String[] args) {
+        double arg1;
+        double arg2;
+        System.out.println("Программа вычисляет квадратный корень");
+        System.out.println("Введите подкоренное значение:");
+        Scanner in = new Scanner(System.in);
+        arg2 = in.nextDouble();
+        arg1 =
+        arg2 = arg2 / arg1 = 1;
+        System.out.println(arg1);}
+    }
